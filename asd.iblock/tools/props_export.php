16596a70815a1a998f6ad752b47bb9fadf741842
Sergey Buynitsky <coder8@web-axioma.ru>
2024-10-21 08:50:50 +0700

    sale 24.200.200

--- asd.iblock/tools/props_export.php
+++ asd.iblock/tools/props_export.php
@@ -3,7 +3,7 @@ $intID = intval($_REQUEST['ID']);
 if (0 < $intID) {
 	include_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/license_key.php';
 	$strPath = $_SERVER['DOCUMENT_ROOT'].'/bitrix/tmp/asd.iblock/';
-	$strName = 'asd_props_export_'.$intID.'_'.md5($LICENSE_KEY).'.xml';
+	$strName = 'asd_props_export_'.$intID.'_'.md5(!empty($LICENSE_KEY) ? $LICENSE_KEY : 'DEMO').'.xml';
 	if (file_exists($strPath.$strName) && is_file($strPath.$strName)) {
 		header('Content-type: text/xml');
 		header('Content-Disposition: attachment; filename="'.$strName.'"');
